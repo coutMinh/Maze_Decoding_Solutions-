8d5693c5999a5b168d69040e638b63e761fa4557
coutMinh <146336712+coutMinh@users.noreply.github.com>
2024-04-27 08:56:45 +0700

    commit(27/4)2

--- mazeGeneration.py
+++ mazeGeneration.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pygame
-
+import random
 class mazeGeneration:
 
     def __init__(self) -> None:
@@ -9,21 +9,39 @@ class mazeGeneration:
         self.A_y = None
         self.B_x = None
         self.B_y = None
-        self.size = 11
+        self.parent = None
+        self.dx = [-2, 0, 0 ,2]
+        self.dy = [0, -2, 2, 0]
+        self.size = 25
+        self.visited = None
 
     def createMaze(self):
+        self.parent = np.array([[None for i in range(self.size)]for j in range(self.size)])
         self.maze = np.array([['x' for i in range(self.size)] for j in range(self.size)])
         self.maze[1:self.size - 1:2, 1:self.size - 1:2] = 'o'
-        visited = np.array([[True for i in range(self.size)]for j in range(self.size)])
-        pos_o = self.maze == 'o'
-        visited[pos_o] = False
-        print(self.maze)
-        print(visited)
-        self.dfs(self.maze)
-        print(self.maze)
+        self.visited = np.array([[False for i in range(self.size)]for j in range(self.size)])
+        #print(self.maze)
+        print(self.visited)
+        self.dfs(1,1)
+        print(self.visited)
+        self.maze[self.visited] = 'o'
+        return self.maze
+
     def dfs(self, s, t):
-        dx = [-2, 0, 0 ,2]
-        dy = [0, -2, 2, 0]
+        self.visited[s, t] = True
+        parent = self.parent[s, t]
+        if(parent != None):
+            self.visited[(s + parent[0])//2, (t + parent[1]) // 2] = True
+        self.maze[s, t] = 'x'
+        way = [0,1,2,3]
+        while(len(way) > 0):
+            k = random.choice(way)
+            way.remove(k)
+            s1 = s + self.dx[k]
+            t1 = t + self.dy[k]
+            if(s1 >= 0 and s1 < self.size and t1 >= 0 and t1 < self.size and self.visited[s1,t1] == False and self.maze[s1, t1] == 'o'):
+                self.parent[s1, t1] = (s, t)
+                self.dfs(s1, t1)
 
 
 def matrix_pygame(maze_matrix: np.array):
@@ -67,4 +85,4 @@ def matrix_pygame(maze_matrix: np.array):
 
 A = mazeGeneration()
 matrix = A.createMaze()
-#matrix_pygame(matrix)
+matrix_pygame(matrix)
